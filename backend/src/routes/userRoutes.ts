638a32909588ca51198d6212f61f92a23aedc2e2
BaGorK <edmealemkassahun@gmail.com>
2024-05-29 01:55:13 +0300

    feat: implement user register

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,7 +1,23 @@
 import express, { Request, Response } from 'express';
+import User from '../models/userModel';
 
 const Router = express.Router();
 
-Router.post('/register', (req: Request, res: Response) => {});
+Router.post('/register', async (req: Request, res: Response) => {
+  try {
+    let user = await User.findOne({
+      email: req.body.email,
+    });
+
+    if (user) return res.status(400).json({ message: 'User already exists' });
+
+    user = await User.create(req.body);
+
+    return res.status(201).json({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: 'Something went wrong' });
+  }
+});
 
 export default Router;
