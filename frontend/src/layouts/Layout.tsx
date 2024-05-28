e46e6d1c2cedf01d30fac4ca74f6643dd328d853
BaGorK <edmealemkassahun@gmail.com>
2024-05-29 01:33:46 +0300

    feat: add prop type | layout.tsx for children

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,12 +1,18 @@
+import { ReactNode } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 
-export default function Layout() {
+interface Props {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: Props) {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
       <Hero />
+      <div className='container mx-auto py-10 flex-1'>{children}</div>
       <Footer />
     </div>
   );
