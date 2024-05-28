ec19eb23b3352dee8853c9ee9087edfacc7b9b30
BaGorK <edmealemkassahun@gmail.com>
2024-05-28 21:31:24 +0300

    feat: routes on app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello tailwind!
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<span>Home page</span>}>
+          Home page
+        </Route>
+        <Route path='/search' element={<span>search page</span>}>
+          Home page
+        </Route>
+        <Route path='/*' element={<Navigate to='/' />}>
+          Home page
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
