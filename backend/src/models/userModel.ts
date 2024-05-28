78f95c7caa3512d2e7ef06742bd747c83967f002
BaGorK <edmealemkassahun@gmail.com>
2024-05-29 02:11:42 +0300

    feat: add a pre save hook on user model

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { hashPassword } from '../utils/passwordUtils';
 
 export type UserType = {
   _id: string;
@@ -13,6 +14,14 @@ const UserSchema = new mongoose.Schema({
   password: { required: true, type: String },
   firstName: { required: true, type: String },
   lastName: { required: true, type: String },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await hashPassword(this.password);
+  }
+
+  next();
 });
 
 const User = mongoose.model<UserType>('User', UserSchema);
