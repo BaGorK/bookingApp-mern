7610afe3d669aa019cdacf88c75f90bed1ad89d7
BaGorK <edmealemkassahun@gmail.com>
2024-05-28 21:51:59 +0300

    feat: add a custom tailwind css class | config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    container: {
+      padding: {
+        md: '10rem' /*you're setting the padding property for medium (md) screens to 10rem. */,
+      },
+    },
   },
   plugins: [],
 };
