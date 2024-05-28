9a83f332b64faaba206d7fc4e97de6943c1cd8de
BaGorK <edmealemkassahun@gmail.com>
2024-05-28 19:06:43 +0300

    feat: basic express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello Backend!')
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server listening on port : ${PORT}`));
