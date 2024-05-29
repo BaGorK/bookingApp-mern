6261065baefc359965786c9c02fde6553b6de4c1
BaGorK <edmealemkassahun@gmail.com>
2024-05-29 20:20:09 +0300

    feat: implement auth middleware function

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,10 +1,32 @@
 import { NextFunction, Request, Response } from 'express';
+import { verifyJWT } from '../utils/tokenUtils';
+import { JwtPayload } from 'jsonwebtoken';
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+    }
+  }
+}
 
 export const verifyToken = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log('verify token middleware');
-  next();
+  const token = req.cookies['auth_token'];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  try {
+    const { userId } = verifyJWT(token) as JwtPayload;
+
+    req.userId = userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
 };
