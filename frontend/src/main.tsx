49af1f6932f5e1303644d067057b864978643146
BaGorK <edmealemkassahun@gmail.com>
2024-05-29 12:07:43 +0300

    feat: main.tsx | update query client default options | add query dev tools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
