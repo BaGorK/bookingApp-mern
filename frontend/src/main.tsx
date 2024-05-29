1d6a24d613fc7cbd4eb8e249cb2ec5a0d066c6b6
BaGorK <edmealemkassahun@gmail.com>
2024-05-29 12:48:03 +0300

    feat: provide app context | main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
+import AppContextProvider from './contexts/AppContext.tsx';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,30 +17,32 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
-      <App />
+    <AppContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <App />
 
-      <Toaster
-        position='top-center'
-        gutter={12}
-        containerStyle={{ margin: '8px' }}
-        toastOptions={{
-          success: {
-            duration: 2000,
-          },
-          error: {
-            duration: 3000,
-          },
-          style: {
-            fontSize: '16px',
-            maxWidth: '500px',
-            padding: '16px 24px',
-            backgroundColor: '#f8fafc',
-            color: '#0f172a',
-          },
-        }}
-      />
-    </QueryClientProvider>
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: {
+              duration: 2000,
+            },
+            error: {
+              duration: 3000,
+            },
+            style: {
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px 24px',
+              backgroundColor: '#f8fafc',
+              color: '#0f172a',
+            },
+          }}
+        />
+      </QueryClientProvider>{' '}
+    </AppContextProvider>
   </React.StrictMode>
 );
