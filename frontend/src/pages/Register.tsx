f2d8a0104531e1af52358fa2bedc560bbc476a8d
BaGorK <edmealemkassahun@gmail.com>
2024-05-29 11:19:59 +0300

    feat: display validation error | using formState

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -9,8 +9,13 @@ type RegisterFormDataType = {
 };
 
 export default function Register() {
-  const { register, handleSubmit, watch } = useForm<RegisterFormDataType>();
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<RegisterFormDataType>();
+  console.log(errors);
   const onSubmit = handleSubmit((data) => {
     console.log(data);
   });
@@ -24,81 +29,106 @@ export default function Register() {
           className='text-gray-700 text-sm font-bold flex-1'
         >
           First Name
+          <input
+            type='text'
+            id='firstName'
+            defaultValue='edmealem'
+            className='border rounded w-full py-1 px-2 font-normal'
+            {...register('firstName', { required: 'this field is required' })}
+          />
+          {errors.firstName && (
+            <p className='text-red-700 text-sm font-normal '>
+              {errors.firstName.message}
+            </p>
+          )}
         </label>
-        <input
-          type='text'
-          id='firstName'
-          defaultValue='edmealem'
-          className='border rounded w-full py-1 px-2 font-normal'
-          {...register('firstName', { required: 'this field is required' })}
-        />
         <label
           htmlFor='lastName'
           className='text-gray-700 flex-1 text-sm font-bold '
         >
           last Name
+          <input
+            type='text'
+            id='lastName'
+            defaultValue='kassahun'
+            {...register('lastName', { required: 'this field is required' })}
+            className='border rounded w-full py-1 px-2 font-normal'
+          />
+          {errors.lastName && (
+            <p className='text-red-700 text-sm font-normal'>
+              {errors.lastName.message}
+            </p>
+          )}
         </label>
-        <input
-          type='text'
-          id='lastName'
-          defaultValue='kassahun'
-          {...register('lastName', { required: 'this field is required' })}
-          className='border rounded w-full py-1 px-2 font-normal'
-        />
       </div>
       <label
         htmlFor='email'
         className='text-gray-700 flex-1 text-sm font-bold '
       >
         email
+        <input
+          type='email'
+          id='email'
+          defaultValue='test@test.com'
+          {...register('email', { required: 'this field is required' })}
+          className='border rounded w-full py-1 px-2 font-normal'
+        />
+        {errors.email && (
+          <p className='text-red-700 text-sm font-normal'>
+            {errors.email.message}
+          </p>
+        )}
       </label>
-      <input
-        type='email'
-        id='email'
-        defaultValue='test@test.com'
-        {...register('email', { required: 'this field is required' })}
-        className='border rounded w-full py-1 px-2 font-normal'
-      />
       <label
         htmlFor='password'
         className='text-gray-700 flex-1 text-sm font-bold '
       >
         password
+        <input
+          type='password'
+          id='password'
+          defaultValue='test1234'
+          {...register('password', {
+            required: 'this field is required',
+            minLength: {
+              value: 6,
+              message: 'password must be at least 6 characters',
+            },
+          })}
+          className='border rounded w-full py-1 px-2 font-normal'
+        />{' '}
+        {errors.password && (
+          <p className='text-red-700 text-sm font-normal'>
+            {errors.password.message}
+          </p>
+        )}
       </label>
-      <input
-        type='password'
-        id='password'
-        defaultValue='test1234'
-        {...register('password', {
-          required: 'this field is required',
-          minLength: {
-            value: 6,
-            message: 'password must be at least 6 characters',
-          },
-        })}
-        className='border rounded w-full py-1 px-2 font-normal'
-      />
       <label
         htmlFor='confirmPassword'
         className='text-gray-700 flex-1 text-sm font-bold '
       >
         confirm password
+        <input
+          type='password'
+          id='confirmPassword'
+          defaultValue='test1234'
+          {...register('confirmPassword', {
+            validate: (val) => {
+              if (!val) {
+                return 'This field is required';
+              } else if (watch('password') !== val) {
+                return 'Your passwords do not match';
+              }
+            },
+          })}
+          className='border rounded w-full py-1 px-2 font-normal'
+        />
+        {errors.confirmPassword && (
+          <p className='text-red-700 text-sm font-normal'>
+            {errors.confirmPassword.message}
+          </p>
+        )}
       </label>
-      <input
-        type='password'
-        id='confirmPassword'
-        defaultValue='test1234'
-        {...register('confirmPassword', {
-          validate: (val) => {
-            if (!val) {
-              return 'This field is required';
-            } else if (watch('password') !== val) {
-              return 'Your passwords do not match';
-            }
-          },
-        })}
-        className='border rounded w-full py-1 px-2 font-normal'
-      />
       <span>
         <button
           type='submit'
