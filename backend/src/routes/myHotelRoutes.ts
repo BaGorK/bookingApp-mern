fe2d4bb315b215d9071b477a5335519097e40942
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-05-30 12:51:27 +0300

    feat: validate body express validator | hotel Model

--- backend/src/routes/myHotelRoutes.ts
+++ backend/src/routes/myHotelRoutes.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 import cloudinary from "cloudinary";
 import Hotel, { HotelType } from "../models/hotelModel";
 import { verifyToken } from "../middlewares/authMiddleware";
+import { body } from "express-validator";
 
 const Router = express.Router();
 
@@ -18,6 +19,23 @@ const upload = multer({
 Router.post(
   "/",
   verifyToken,
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("description").notEmpty().withMessage("description is required"),
+    body("type").notEmpty().withMessage("type is required"),
+    body("pricePerNight")
+      .notEmpty()
+      .isNumeric()
+      .withMessage(
+        "pricePerNight is required and  pricePerNight must be a number"
+      ),
+    body("facilities")
+      .notEmpty()
+      .isArray()
+      .withMessage("facilities are required and facilities must be an array"),
+  ],
   upload.array("imageFiles", 6),
   async (req: Request, res: Response) => {
     try {
