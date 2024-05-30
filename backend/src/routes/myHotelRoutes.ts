df2d4bd6f124edb66f609b98130e8fbc6c6b6bd6
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-05-30 11:38:24 +0300

    feat: multer setup

--- backend/src/routes/myHotelRoutes.ts
+++ backend/src/routes/myHotelRoutes.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from "express";
+import multer from "multer";
 
 const Router = express.Router();
 
-// api/v1/myHotels
-Router.post("/", async (req: Request, res: Response) => {
-  return res.status(200).send("myHotels route");
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
 });
+
+// api/v1/myHotels
+Router.post(
+  "/",
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
+    return res.status(200).send("myHotels route");
+  }
+);
 
 export default Router;
