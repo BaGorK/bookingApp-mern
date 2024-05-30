e0caa8408499569dd28fdb3a273552acaa4baa1c
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-05-30 12:10:03 +0300

    feat: hotelSchema defined

--- backend/src/models/hotelModel.ts
+++ backend/src/models/hotelModel.ts
@@ -17,8 +17,22 @@ export type HotelType = {
   lastUpdated: Date;
 };
 
-const HotelSchema = new mongoose.Schema<HotelType>({});
+const HotelSchema = new mongoose.Schema<HotelType>({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  description: { type: String, required: true },
+  type: { type: String, required: true },
+  adultCount: { type: Number, required: true },
+  childCount: { type: Number, required: true },
+  facilities: [{ type: String, required: true }],
+  pricePerNight: { type: Number, required: true },
+  starRating: { type: Number, required: true, min: 1, max: 5 },
+  imageUrls: [{ type: String, required: true }],
+  lastUpdated: { type: Date, required: true },
+});
 
-const Hotel = mongoose.model("Hotel", HotelSchema);
+const Hotel = mongoose.model<HotelType>("Hotel", HotelSchema);
 
 export default Hotel;
