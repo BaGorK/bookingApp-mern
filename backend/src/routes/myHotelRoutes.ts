f20aa5f4feee97333f2885c060ce88235645de54
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-05-30 11:49:37 +0300

    feat: upload images | get image urls

--- backend/src/routes/myHotelRoutes.ts
+++ backend/src/routes/myHotelRoutes.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
+import cloudinary from "cloudinary";
 
 const Router = express.Router();
 
@@ -16,7 +17,28 @@ Router.post(
   "/",
   upload.array("imageFiles", 6),
   async (req: Request, res: Response) => {
-    return res.status(200).send("myHotels route");
+    try {
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel = req.body;
+
+      // 1. upload image to cloudinary
+      const uploadPromises = imageFiles.map(async (image) => {
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        let dataURI = `data:${image.mimetype};base64,${b64}`;
+        const res = await cloudinary.v2.uploader.upload(dataURI);
+
+        return res.url;
+      });
+
+      const imageUrls = await Promise.all(uploadPromises)
+      // 2. if (upload) add the urls to the new hotel
+      // 3. save the new hotel in our database
+
+      // 4. return a 201 status
+      return res
+        .status(201)
+        .json({ message: "hotel created success", data: newHotel });
+    } catch (error) {}
   }
 );
 
