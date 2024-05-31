b5b645bc13e4325e3380f77aa12658972a5a6b97
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-05-31 20:54:37 +0300

    style: type section radio buttons

--- frontend/src/components/forms/manageHotelForm/TypeSection.tsx
+++ frontend/src/components/forms/manageHotelForm/TypeSection.tsx
@@ -1,15 +1,23 @@
 import { useFormContext } from "react-hook-form";
 import { hotelTypes } from "../../../config/hotel-options-config";
+import { HotelFormDataType } from "./ManageHotelForm";
 
 export default function TypeSection() {
-  const { register } = useFormContext();
+  const { register, watch } = useFormContext<HotelFormDataType>();
+  const typeWatch = watch("type");
 
   return (
     <div>
       <h2 className="text-2xl font-bold mb-3">Type</h2>
       <div className="grid grid-cols-5 gap-2">
         {hotelTypes.map((type) => (
-          <label>
+          <label
+            className={
+              typeWatch === type
+                ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold"
+                : "cursor-pointer bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold"
+            }
+          >
             <input
               type="radio"
               value={type}
