82a0899165ebfea27f91656c1a4bbaf5a09dd80e
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-05-31 19:58:37 +0300

    feat: add a type HotelFormDataType

--- frontend/src/components/forms/manageHotelForm/ManageHotelForm.tsx
+++ frontend/src/components/forms/manageHotelForm/ManageHotelForm.tsx
@@ -1,3 +1,18 @@
+export type HotelFormDataType = {
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  pricePerNight: number;
+  starRating: number;
+  facilities: string[];
+  imageFiles: FileList;
+  imageUrls: string[];
+  adultCount: number;
+  childCount: number;
+};
+
 function ManageHotelForm() {
   return <div>ManageHotelForm</div>;
 }
