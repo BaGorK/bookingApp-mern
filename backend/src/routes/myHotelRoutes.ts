bb9e673f4b0a06708998c11b1391100d19e12ac0
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-01 00:41:49 +0300

    feat: implement getAllHotels fuctionality

--- backend/src/routes/myHotelRoutes.ts
+++ backend/src/routes/myHotelRoutes.ts
@@ -72,4 +72,10 @@ Router.post(
   }
 );
 
+Router.get('/', verifyToken, async (req:Request, res: Response) => {
+  try{
+    const hotels = await Hotel.find({userId: req.userId});
+    res.status(200).json({data: hotels});
+  }catch(err){res.status(500).json({message: 'Error fetching hotels'})}
+})
 export default Router;
