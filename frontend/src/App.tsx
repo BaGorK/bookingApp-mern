1be21a72108fceb8320d25a129c23d43ad47bda8
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-05-31 18:20:48 +0300

    feat: update app routes | add add-hotel route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Layout from './layouts/Layout';
-import Register from './pages/Register';
-import SignIn from './pages/SignIn';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+import { useAppContext } from "./contexts/AppContext";
+import AddHotel from "./pages/AddHotel";
 
 export default function App() {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout>Home page</Layout>} />
-        <Route path='/search' element={<Layout>search page</Layout>} />
+        <Route path="/" element={<Layout>Home page</Layout>} />
+        <Route path="/search" element={<Layout>search page</Layout>} />
         <Route
-          path='/register'
+          path="/register"
           element={
             <Layout>
               <Register />
@@ -18,14 +22,26 @@ export default function App() {
           }
         />
         <Route
-          path='/sign-in'
+          path="/sign-in"
           element={
             <Layout>
               <SignIn />
             </Layout>
           }
         />
-        <Route path='/*' element={<Navigate to='/' />} />
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+          </>
+        )}
+        <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
