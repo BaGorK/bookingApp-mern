9bce2d2a2d89c2dd1877078db47933926f10ab3b
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-09 14:55:46 +0300

    feat: useMutation on  EditHotel page and pass to manageHotelForm component

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -1,8 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import * as apiClient from '../services/api-client';
 import { HotelType } from '../../../backend/src/shared/types';
 import ManageHotelForm from '../components/forms/manageHotelForm/ManageHotelForm';
+import toast from 'react-hot-toast';
 
 export default function EditHotel() {
   const { hotelId } = useParams<{ hotelId: string }>();
@@ -14,9 +15,27 @@ export default function EditHotel() {
 
   const { data: hotelData } = data as { data?: HotelType };
 
+  const { mutate, isPending } = useMutation({
+    mutationFn: apiClient.updateMyHotelById,
+    onSuccess: () => {
+      toast.success('Hotel updated successfully');
+    },
+    onError: (error: Error) => {
+      toast.error(error.message);
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
   return (
     <>
-      <ManageHotelForm hotel={hotelData} />
+      <ManageHotelForm
+        isPending={isPending}
+        onSaveHotel={handleSave}
+        hotel={hotelData}
+      />
     </>
   );
 }
