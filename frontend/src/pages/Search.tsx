6c1856363fd410551678c61e898f1d6d0a0ac07b
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-22 01:31:13 +0300

    feat: search page | using query to fetch hotels

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,9 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import { useSearchContext } from '../contexts/SearchContext';
+import * as apiClient from '../services/api-client';
+import { useState } from 'react';
 
 export default function Search() {
   const search = useSearchContext();
+  const [page, setPage] = useState<number>(1);
 
-  console.log(search);
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString(),
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['searchHotels', searchParams],
+    queryFn: () => apiClient.searchHotels(searchParams),
+  });
+
+  console.log(isLoading, data);
 
   return <div>Search page</div>;
 }
