2e194c73fe8c9d46f1d4ae7ffb9775037d27ef17
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-22 02:02:25 +0300

    feat: implement the search page ui

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useSearchContext } from '../contexts/SearchContext';
 import * as apiClient from '../services/api-client';
 import { useState } from 'react';
+import SearchResultCard from '../components/SearchResultsCard';
 
 export default function Search() {
   const search = useSearchContext();
@@ -16,12 +17,32 @@ export default function Search() {
     page: page.toString(),
   };
 
-  const { data, isLoading } = useQuery({
+  const { data: hotelsDate, isLoading } = useQuery({
     queryKey: ['searchHotels', searchParams],
     queryFn: () => apiClient.searchHotels(searchParams),
   });
 
-  console.log(isLoading, data);
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
+      <div className='rounded-lg border border-slate-300 p-5 h-fit sticky top-10 '>
+        <div className='space-y-5'>
+          <h3 className='text-lg font-semibold border-b border-slate-300 pb-5'>
+            Filter by:
+          </h3>
+        </div>
+      </div>
 
-  return <div>Search page</div>;
+      <div className='flex flex-col gap-5'>
+        <div className='flex justify-between items-center'>
+          <span className='text-xl font-bold'>
+            {hotelsDate?.pagination.total} Hotels found
+            {search.destination ? ` in ${search.destination}` : ''}
+          </span>
+        </div>
+        {hotelsDate?.data.map((hotel) => (
+          <SearchResultCard key={hotel._id} hotel={hotel} />
+        ))}
+      </div>
+    </div>
+  );
 }
