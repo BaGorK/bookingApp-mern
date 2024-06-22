a3fc0066e457b85c17309a81498254b951cb8ae6
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-22 14:28:53 +0300

    feat: implement hotel detail page

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
 import * as apiClient from './../services/api-client';
+import { AiFillStar } from 'react-icons/ai';
 
 export default function Detail() {
   const { hotelId } = useParams();
@@ -13,7 +14,48 @@ export default function Detail() {
 
   if (isLoading) return <div>Loading...</div>;
 
-  console.log(hotel);
+  if (!hotel) return <></>;
 
-  return <div>Detail</div>;
+  return (
+    <div className='space-y-6'>
+      <div>
+        <span className='flex'>
+          {Array.from({ length: hotel.starRating }).map((_, i) => (
+            <AiFillStar className='fill-yellow-400' key={i} />
+          ))}
+        </span>
+        <h1 className='text-3xl font-bold'>{hotel.name}</h1>
+      </div>
+
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+        {hotel.imageUrls.map((image: string) => (
+          <div className='h-[300px]' key={image}>
+            <img
+              src={image}
+              alt={hotel.name}
+              className='rounded-md w-full h-full object-cover object-center'
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className='grid grid-cols-1 lg:grid-cols-4 gap-2'>
+        {hotel.facilities.map((facility: string) => (
+          <div key={facility} className='border border-slate-300 rounded-sm p-3'>
+            {facility}
+          </div>
+        ))}
+      </div>
+
+      <div className='grid grid-cols-1 lg:grid-cols-[2fr_1fr]'>
+        <div className='whitespace-pre-line'>{hotel.description}</div>
+        <div className='h-fit'>
+          {/* <GuestInfoForm
+            pricePerNight={hotel.pricePerNight}
+            hotelId={hotel._id}
+          /> */}
+        </div>
+      </div>
+    </div>
+  );
 }
