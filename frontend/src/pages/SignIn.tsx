ed67fe797891df6480533ebb5ba5da08d0a0433e
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-22 14:50:45 +0300

    feat: sign in page | use location to navigate back to details page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,8 +1,8 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useForm } from "react-hook-form";
-import * as apiClient from "../services/api-client";
-import toast from "react-hot-toast";
-import { Link, useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import * as apiClient from '../services/api-client';
+import toast from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export type SignInFormDataType = {
   email: string;
@@ -11,7 +11,8 @@ export type SignInFormDataType = {
 
 export default function SignIn() {
   const navigate = useNavigate();
-
+  const location = useLocation();
+  
   const queryClient = useQueryClient();
 
   const {
@@ -23,11 +24,11 @@ export default function SignIn() {
   const { mutate, isPending } = useMutation({
     mutationFn: apiClient.signIn,
     onSuccess: () => {
-      toast.success("Login successful");
+      toast.success('Login successful');
       queryClient.invalidateQueries({
-        queryKey: ["validateToken"],
+        queryKey: ['validateToken'],
       });
-      navigate("/");
+      navigate(location.state?.from?.pathname || '/');
     },
     onError: (error) => {
       toast.error(error.message);
@@ -39,62 +40,62 @@ export default function SignIn() {
   });
 
   return (
-    <form onSubmit={handleOnSubmit} className="flex flex-col gap-5">
-      <h2 className="text-3xl font-bold">Sign In</h2>
+    <form onSubmit={handleOnSubmit} className='flex flex-col gap-5'>
+      <h2 className='text-3xl font-bold'>Sign In</h2>
 
       <label
-        htmlFor="email"
-        className="text-gray-700 flex-1 text-sm font-bold "
+        htmlFor='email'
+        className='text-gray-700 flex-1 text-sm font-bold '
       >
         email
         <input
-          type="email"
-          id="email"
-          defaultValue="test@test.com"
-          {...register("email", { required: "this field is required" })}
-          className="border rounded w-full py-1 px-2 font-normal"
+          type='email'
+          id='email'
+          defaultValue='test@test.com'
+          {...register('email', { required: 'this field is required' })}
+          className='border rounded w-full py-1 px-2 font-normal'
         />
         {errors.email && (
-          <p className="text-red-700 text-sm font-normal">
+          <p className='text-red-700 text-sm font-normal'>
             {errors.email.message}
           </p>
         )}
       </label>
       <label
-        htmlFor="password"
-        className="text-gray-700 flex-1 text-sm font-bold "
+        htmlFor='password'
+        className='text-gray-700 flex-1 text-sm font-bold '
       >
         password
         <input
-          type="password"
-          id="password"
-          defaultValue="test1234"
-          {...register("password", {
-            required: "this field is required",
+          type='password'
+          id='password'
+          defaultValue='test1234'
+          {...register('password', {
+            required: 'this field is required',
             minLength: {
               value: 6,
-              message: "password must be at least 6 characters",
+              message: 'password must be at least 6 characters',
             },
           })}
-          className="border rounded w-full py-1 px-2 font-normal"
-        />{" "}
+          className='border rounded w-full py-1 px-2 font-normal'
+        />{' '}
         {errors.password && (
-          <p className="text-red-700 text-sm font-normal">
+          <p className='text-red-700 text-sm font-normal'>
             {errors.password.message}
           </p>
         )}
       </label>
-      <span className="flex  flex-col justify-start gap-3">
+      <span className='flex  flex-col justify-start gap-3'>
         <button
-          type="submit"
+          type='submit'
           disabled={isPending}
-          className="bg-blue-600 text-white  px-3 py-2 rounded font-bold hover:bg-blue-500 text-xl"
+          className='bg-blue-600 text-white  px-3 py-2 rounded font-bold hover:bg-blue-500 text-xl'
         >
-          {isPending ? "Signing In ..." : "Sign In"}
+          {isPending ? 'Signing In ...' : 'Sign In'}
         </button>
-        <span className="text-sm">
-          Not Registered?{" "}
-          <Link to="/register" className="text-blue-600 underline">
+        <span className='text-sm'>
+          Not Registered?{' '}
+          <Link to='/register' className='text-blue-600 underline'>
             Create an account here
           </Link>
         </span>
