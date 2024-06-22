dac16c92e9abfb00c64e7d24953ee9dfbcc3d5bd
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-22 11:58:43 +0300

    feat: update hotels route | search options

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -26,12 +26,55 @@ const constructSearchQuery = (queryParams: any) => {
     };
   }
 
+  if (queryParams.facilities) {
+    constructedQuery.facilities = {
+      $all: Array.isArray(queryParams.facilities)
+        ? queryParams.facilities
+        : [queryParams.facilities],
+    };
+  }
+
+  if (queryParams.types) {
+    constructedQuery.type = {
+      $in: Array.isArray(queryParams.types)
+        ? queryParams.types
+        : [queryParams.types],
+    };
+  }
+
+  if (queryParams.stars) {
+    const starRatings = Array.isArray(queryParams.stars)
+      ? queryParams.stars.map((star: string) => parseInt(star))
+      : parseInt(queryParams.stars);
+
+    constructedQuery.starRating = { $in: starRatings };
+  }
+
+  if (queryParams.maxPrice) {
+    constructedQuery.pricePerNight = {
+      $lte: parseInt(queryParams.maxPrice).toString(),
+    };
+  }
+
   return constructedQuery;
 };
 
 router.get('/search', async (req: Request, res: Response) => {
   try {
     const query = constructSearchQuery(req.query);
+
+    let sortOptions = {};
+    switch (req.query.sortOption) {
+      case 'starRating':
+        sortOptions = { starRating: -1 };
+        break;
+      case 'pricePerNightAsc':
+        sortOptions = { pricePerNight: 1 };
+        break;
+      case 'pricePerNightDesc':
+        sortOptions = { pricePerNight: -1 };
+        break;
+    }
 
     const pageSize = 5;
     const pageNumber = parseInt(
@@ -40,7 +83,10 @@ router.get('/search', async (req: Request, res: Response) => {
 
     const skip = (pageNumber - 1) * pageSize;
 
-    const hotels = await Hotel.find(query).skip(skip).limit(pageSize);
+    const hotels = await Hotel.find(query)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(pageSize);
 
     const total = await Hotel.countDocuments(query);
 
