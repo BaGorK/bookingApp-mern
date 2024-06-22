341af51016ad5d42ad3611a542ca49cfd4841abc
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-22 12:34:58 +0300

    feat: add a StarRatingFilter in the search page

--- frontend/src/components/StarRatingFilter.tsx
+++ frontend/src/components/StarRatingFilter.tsx
@@ -4,5 +4,21 @@ type Props = {
 };
 
 export default function StarRatingFilter({ selectedStars, onChange }: Props) {
-  return <div>StarRatingFilter</div>;
+  return (
+    <div className='border-b border-slate-300 pb-5'>
+      <h4 className='text-md font-semibold mb-2'>Property Rating</h4>
+      {['5', '4', '3', '2', '1'].map((star) => (
+        <label className='flex items-center space-x-2'>
+          <input
+            type='checkbox'
+            className='rounded'
+            value={star}
+            checked={selectedStars.includes(star)}
+            onChange={onChange}
+          />
+          <span>{star} Stars</span>
+        </label>
+      ))}
+    </div>
+  );
 }
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -4,10 +4,12 @@ import * as apiClient from '../services/api-client';
 import { useState } from 'react';
 import SearchResultCard from '../components/SearchResultsCard';
 import Pagination from '../components/Pagination';
+import StarRatingFilter from '../components/StarRatingFilter';
 
 export default function Search() {
   const search = useSearchContext();
   const [page, setPage] = useState<number>(1);
+  const [selectedStars, setSelectedStars] = useState<string[]>([]);
 
   const searchParams = {
     destination: search.destination,
@@ -16,6 +18,7 @@ export default function Search() {
     adultCount: search.adultCount.toString(),
     childCount: search.childCount.toString(),
     page: page.toString(),
+    stars: selectedStars,
   };
 
   const { data: hotelsDate, isLoading } = useQuery({
@@ -27,6 +30,16 @@ export default function Search() {
     setPage(page);
   };
 
+  const handleStarsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const starRating = e.target.value;
+
+    setSelectedStars((prevStars) =>
+      e.target.checked
+        ? [...prevStars, starRating]
+        : prevStars.filter((star) => star !== starRating)
+    );
+  };
+
   // if (isLoading || !hotelsDate) return <div>Loading...</div>;
 
   return (
@@ -36,6 +49,10 @@ export default function Search() {
           <h3 className='text-lg font-semibold border-b border-slate-300 pb-5'>
             Filter by:
           </h3>
+          <StarRatingFilter
+            selectedStars={selectedStars}
+            onChange={handleStarsChange}
+          />
         </div>
       </div>
 
