3b2c8792e484bbdd33a57959df31494a414b626d
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-23 15:39:21 +0300

    feat: save search form values to the session storage

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -22,12 +22,26 @@ type Props = {
   children: ReactNode;
 };
 export const SearchContextProvider = ({ children }: Props) => {
-  const [destination, setDestination] = useState<string>('');
-  const [checkIn, setCheckIn] = useState<Date>(new Date());
-  const [checkOut, setCheckOut] = useState<Date>(new Date());
-  const [adultCount, setAdultCount] = useState<number>(1);
-  const [childCount, setChildCount] = useState<number>(0);
-  const [hotelId, setHotelId] = useState<string>('');
+  const [destination, setDestination] = useState<string>(
+    () => sessionStorage.getItem('destination') || ''
+  );
+  const [checkIn, setCheckIn] = useState<Date>(
+    () =>
+      new Date(sessionStorage.getItem('checkIn') || new Date().toISOString())
+  );
+  const [checkOut, setCheckOut] = useState<Date>(
+    () =>
+      new Date(sessionStorage.getItem('checkOut') || new Date().toISOString())
+  );
+  const [adultCount, setAdultCount] = useState<number>(() =>
+    parseInt(sessionStorage.getItem('adultCount') || '1')
+  );
+  const [childCount, setChildCount] = useState<number>(() =>
+    parseInt(sessionStorage.getItem('adultCount') || '0')
+  );
+  const [hotelId, setHotelId] = useState<string>(
+    sessionStorage.getItem('hotelId') || ''
+  );
 
   const saveSearchValues = (
     destination: string,
@@ -42,7 +56,15 @@ export const SearchContextProvider = ({ children }: Props) => {
     setCheckOut(checkOut);
     setAdultCount(adultCount);
     setChildCount(childCount);
-    setHotelId(hotelId || '');
+    if (hotelId) setHotelId(hotelId);
+
+    sessionStorage.setItem('destination', destination);
+    sessionStorage.setItem('checkIn', checkIn.toISOString());
+    sessionStorage.setItem('checkOut', checkOut.toISOString());
+    sessionStorage.setItem('adultCount', adultCount.toString());
+    sessionStorage.setItem('childCount', childCount.toString());
+
+    if(hotelId) sessionStorage.setItem('hotelId', hotelId);
   };
 
   return (
