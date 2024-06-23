0df2b7e36ebf2b0e9e08d3ae93a9319bf730f56a
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-23 15:51:25 +0300

    feat: add a get me route | controller

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,8 +2,26 @@ import express, { Request, Response } from 'express';
 import User from '../models/userModel';
 import { createJWT } from '../utils/tokenUtils';
 import { check, validationResult } from 'express-validator';
+import { verifyToken } from '../middlewares/authMiddleware';
 
 const Router = express.Router();
+
+Router.get('/me', verifyToken, async (req: Request, res: Response) => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById(userId).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+});
 
 // /api/users/register
 Router.post(
