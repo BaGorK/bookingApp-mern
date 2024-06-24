d3b6db1bbbd4d811c8b8979acd29105179c6cc30
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-24 04:13:17 +0300

    update: types | add booking type | payment intent res type

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -13,6 +13,7 @@ export type HotelType = {
   starRating: number;
   imageUrls: string[];
   lastUpdated: Date;
+  bookings: BookingType[];
 };
 
 export type UserType = {
@@ -30,4 +31,23 @@ export type HotelSearchResponse = {
     page: number;
     pages: number;
   };
+};
+
+export type PaymentIntentResponse = {
+  paymentIntentId: string;
+  clientSecret: string;
+  totalCost: number;
+};
+
+export type BookingType = {
+  _id: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  adultCount: number;
+  childCount: number;
+  checkIn: Date;
+  checkOut: Date;
+  totalCost: number;
 };
