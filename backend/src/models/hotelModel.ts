c8a452be5201b7fe88bb8fac7e96308ab1f4968f
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-24 04:12:35 +0300

    feat: hotel model | add a booking Schema

--- backend/src/models/hotelModel.ts
+++ backend/src/models/hotelModel.ts
@@ -1,5 +1,17 @@
-import mongoose from "mongoose";
-import { HotelType } from "../shared/types";
+import mongoose from 'mongoose';
+import { BookingType, HotelType } from '../shared/types';
+
+const bookingSchema = new mongoose.Schema<BookingType>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  adultCount: { type: Number, required: true },
+  childCount: { type: Number, required: true },
+  checkIn: { type: Date, required: true },
+  checkOut: { type: Date, required: true },
+  userId: { type: String, required: true },
+  totalCost: { type: Number, required: true },
+});
 
 const HotelSchema = new mongoose.Schema<HotelType>({
   userId: { type: String, required: true },
@@ -15,8 +27,9 @@ const HotelSchema = new mongoose.Schema<HotelType>({
   starRating: { type: Number, required: true, min: 1, max: 5 },
   imageUrls: [{ type: String, required: true }],
   lastUpdated: { type: Date, required: true },
+  bookings: [bookingSchema]
 });
 
-const Hotel = mongoose.model<HotelType>("Hotel", HotelSchema);
+const Hotel = mongoose.model<HotelType>('Hotel', HotelSchema);
 
 export default Hotel;
