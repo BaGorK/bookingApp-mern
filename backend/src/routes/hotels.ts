3fa8453acbb15815b85c33a5cc1d43d424774a5d
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-06-24 04:08:04 +0300

    feat: hotel route | add new route to create a hotel booking

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -2,7 +2,11 @@ import express, { Request, Response } from 'express';
 import Stripe from 'stripe';
 
 import Hotel from '../models/hotelModel';
-import { HotelSearchResponse, PaymentIntentResponse } from '../shared/types';
+import {
+  BookingType,
+  HotelSearchResponse,
+  PaymentIntentResponse,
+} from '../shared/types';
 import { param, validationResult } from 'express-validator';
 import { verifyToken } from '../middlewares/authMiddleware';
 
@@ -168,6 +172,57 @@ router.post(
     };
 
     res.status(200).json(response);
+  }
+);
+
+router.post(
+  '/:hotelId/bookings',
+  verifyToken,
+  async (req: Request, res: Response) => {
+    try {
+      const paymentIntentId = req.body.paymentIntentId;
+
+      const paymentIntent = await stripe.paymentIntents.retrieve(
+        paymentIntentId as string
+      );
+
+      if (!paymentIntent) {
+        return res.status(400).json({ message: 'payment intent not found' });
+      }
+
+      if (
+        paymentIntent.metadata.hotelId !== req.params.hotelId ||
+        paymentIntent.metadata.userId !== req.userId
+      ) {
+        return res.status(400).json({ message: 'payment intent mismatch' });
+      }
+
+      if (paymentIntent.status !== 'succeeded') {
+        return res.status(400).json({
+          message: `payment intent not succeeded. status: ${paymentIntent.status}`,
+        });
+      }
+
+      const newBooking: BookingType = {
+        ...req.body,
+        userId: req.userId,
+      };
+
+      const hotel = await Hotel.findOneAndUpdate(
+        { _id: req.params.hotelId },
+        { $push: { bookings: newBooking } }
+      );
+
+      if(!hotel) {
+        return res.status(400).json({message: 'hotel not found'})
+      }
+
+      await hotel.save();
+      res.status(200).send()
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'something went wrong' });
+    }
   }
 );
 
